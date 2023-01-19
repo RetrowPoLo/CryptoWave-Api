92440f47e8e8265bc90698760434c6593af17cf1
RetrowPoLo <kantin.visset44@gmail.com>
2023-01-19 11:59:30 +0100

    remove child process for simple git

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const fs = require('fs');
-const { exec } = require('child_process');
+import express from 'express';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 
 // Create a new Express app
 const app = express();
@@ -8,79 +7,12 @@ const app = express();
 // Initialize an array to store the names of missing cryptos
 let missingCryptos = [];
 
-// Function to switch to the "missing-cryptos" branch
-async function gitSwitchBranch() {
-	exec('git checkout test-branch', (error, stdout, stderr) => {
-		if (error && error.message.includes('Already')) {
-			return;
-		} else {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.error(`stderr: ${stderr}`);
-	});
-}
-
-// Function to pull the latest changes
-async function gitPullChanges() {
-	exec('git pull', (error, stdout, stderr) => {
-		if (error && error.message.includes('Already')) {
-			return;
-		} else {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.error(`stderr: ${stderr}`);
-	});
-}
-
-// Function to add and commit the "missing-cryptos.txt" file
-async function gitAddCommit() {
-	exec('git add missing-cryptos.txt', (error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.error(`stderr: ${stderr}`);
-	});
-	exec('git commit -m "Add missing crypto icons"', (error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.error(`stderr: ${stderr}`);
-	});
-}
-
-// Function to push the changes to the remote repository
-async function gitPushChanges() {
-	exec('git push origin test-branch', (error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.error(`stderr: ${stderr}`);
-	});
-}
-
-if (fs.existsSync('missing-cryptos.txt')) {
+if (existsSync('missing-cryptos.txt')) {
 	// If the file exists, read the contents and split it into an array of strings
-	missingCryptos = fs.readFileSync('missing-cryptos.txt', 'utf8').split('\n');
+	missingCryptos = readFileSync('missing-cryptos.txt', 'utf8').split('\n');
 }
 
 app.get('/icon/:symbol', async (req, res) => {
-	console.log('================');
-	console.log('Switch branch');
-	await gitSwitchBranch();
-	console.log('================');
-	console.log('Pull');
-	await gitPullChanges();
-
 	// Get the crypto symbol from the request parameters
 	const symbol = req.params.symbol;
 
@@ -88,7 +20,7 @@ app.get('/icon/:symbol', async (req, res) => {
 	const filePath = __dirname + '/Icons/' + symbol + '.png';
 
 	// Check if the icon file exists
-	if (fs.existsSync(filePath)) {
+	if (existsSync(filePath)) {
 		// If the file exists, send it as a response with cache-control headers set to cache the file for one day
 		res.setHeader('Cache-Control', 'public, max-age=86400');
 		res.sendFile(filePath);
@@ -97,19 +29,11 @@ app.get('/icon/:symbol', async (req, res) => {
 		if (!missingCryptos.includes(symbol)) {
 			// If the symbol is not in the array, add it to the array and write the array to the 'missing-cryptos.txt' file
 			missingCryptos.push(symbol);
-			fs.writeFileSync('missing-cryptos.txt', missingCryptos.join('\n'));
+			writeFileSync('missing-cryptos.txt', missingCryptos.join('\n'));
 		}
 		// Send the 'generic.png' icon as a response with cache-control headers set to cache the file for one day
 		res.setHeader('Cache-Control', 'public, max-age=86400');
 		res.sendFile(__dirname + '/Icons/generic.png');
-
-		console.log('================');
-		console.log('Add then commit');
-		await gitAddCommit();
-		console.log('================');
-		console.log('Push');
-		await gitPushChanges();
-		console.log('================');
 	}
 });
 
