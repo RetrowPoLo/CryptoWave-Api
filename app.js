59a7e2b0e6dbff0202b849b6a8ab31d3306d7164
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2023-01-23 01:20:01 +0100

    Test branch (#4)
    * test commit
    
    * test commit
    
    * Update missing-cryptos.txt
    
    * Change
    
    * Add missing crypto icons
    
    * Empty missing cryptos file
    
    * Change to checkout before trying to add/commit
    
    * Add missing crypto icons
    
    * Changes
    
    * remove child process for simple git
    
    * use simple git
    
    * use simple git
    
    * Add missing crypto icons
    
    * Add missing crypto icons
    
    * Error handling
    
    * Add missing crypto icons
    
    * After test change
    
    * Add missing crypto icons
    
    * Root get path + npm start
    
    * change branch name

--- app.js
+++ app.js
@@ -1,64 +1,95 @@
 const express = require('express');
 const fs = require('fs');
-const { Octokit } = require('@octokit/rest');
-
-require('dotenv').config();
+const simpleGit = require('simple-git');
 
 // Create a new Express app
 const app = express();
 
-const octokit = new Octokit({
-	auth: process.env.GITHUB_TOKEN,
-});
-
-const owner = 'RetrowPoLo';
-const repo = 'CryptoWave-Api';
-const path = 'missing-cryptos.txt';
-const message = 'Add missing crypto icons';
-const content = Buffer.from(fs.readFileSync('missing-cryptos.txt')).toString('base64');
-const branch = 'missing-cryptos';
-
 // Initialize an array to store the names of missing cryptos
 let missingCryptos = [];
+let githubBranch = 'missing-cryptos';
 
 if (fs.existsSync('missing-cryptos.txt')) {
 	// If the file exists, read the contents and split it into an array of strings
 	missingCryptos = fs.readFileSync('missing-cryptos.txt', 'utf8').split('\n');
 }
 
-app.get('/icon/:symbol', (req, res) => {
-	// Get the crypto symbol from the request parameters
-	const symbol = req.params.symbol;
+// Async function to switch to the specified branch
+async function gitSwitchBranch() {
+	await simpleGit().checkout(githubBranch);
+}
 
-	// Construct the path to the crypto icon file
-	const filePath = __dirname + '/Icons/' + symbol + '.png';
+// Async function to pull the latest changes from the specified branch
+async function gitPullChanges() {
+	await simpleGit().pull();
+}
 
-	// Check if the icon file exists
-	if (fs.existsSync(filePath)) {
-		// If the file exists, send it as a response with cache-control headers set to cache the file for one day
-		res.setHeader('Cache-Control', 'public, max-age=86400');
-		res.sendFile(filePath);
-	} else {
-		// If the file does not exist, check if the symbol is already in the missingCryptos array
-		if (!missingCryptos.includes(symbol)) {
-			// If the symbol is not in the array, add it to the array and write the array to the 'missing-cryptos.txt' file
-			missingCryptos.push(symbol);
-			fs.writeFileSync('missing-cryptos.txt', missingCryptos.join('\n'));
+// Async function to add and commit a missing crypto icon file
+async function gitAddCommit() {
+	await simpleGit().add('missing-cryptos.txt');
+	await simpleGit().commit('Add missing crypto icons');
+}
+
+// Async function to push the changes to the specified branch
+async function gitPushChanges() {
+	await simpleGit().push('origin', githubBranch);
+}
+
+// Async Function to handle git actions and catch any errors
+async function handleGitAction(gitAction) {
+	try {
+		await gitAction();
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ error: 'Failed to execute git action' });
+	}
+}
+
+// GET route for the root path
+app.get('/', (req, res) => {
+	res.send('CryptoWaveAPI is running ✅ ');
+});
+
+// GET route for the icon path
+app.get('/icon/:symbol', async (req, res) => {
+	try {
+		// Get the crypto symbol from the request parameters
+		const symbol = req.params.symbol;
+
+		// Construct the path to the crypto icon file
+		const filePath = __dirname + '/Icons/' + symbol + '.png';
+
+		// Check if the icon file exists
+		if (fs.existsSync(filePath)) {
+			// If the file exists, send it as a response with cache-control headers set to cache the file for one day
+			res.setHeader('Cache-Control', 'public, max-age=86400');
+			res.sendFile(filePath);
+		} else {
+			// Switch the git branch
+			await handleGitAction(gitSwitchBranch);
+
+			// Pull any changes from the current git branch
+			await handleGitAction(gitPullChanges);
+
+			// If the file does not exist, check if the symbol is already in the missingCryptos array
+			if (!missingCryptos.includes(symbol)) {
+				// If the symbol is not in the array, add it to the array and write the array to the 'missing-cryptos.txt' file
+				missingCryptos.push(symbol);
+				fs.writeFileSync('missing-cryptos.txt', missingCryptos.join('\n'));
+			}
+			// Send the 'generic.png' icon as a response with cache-control headers set to cache the file for one day
+			res.setHeader('Cache-Control', 'public, max-age=86400');
+			res.sendFile(__dirname + '/Icons/generic.png');
+
+			// Add and commit file to the branch
+			await handleGitAction(gitAddCommit);
+
+			// Push the change to the branch
+			await handleGitAction(gitPushChanges);
 		}
-
-		// Push the updated 'missing-cryptos.txt' to the github repo in a specific branch
-		octokit.repos.createOrUpdateFileContents({
-			owner,
-			repo,
-			path,
-			message,
-			content,
-			branch,
-		});
-
-		// Send the 'generic.png' icon as a response with cache-control headers set to cache the file for one day
-		res.setHeader('Cache-Control', 'public, max-age=86400');
-		res.sendFile(__dirname + '/Icons/generic.png');
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ error: 'An error occurred while handling the request' });
 	}
 });
 
