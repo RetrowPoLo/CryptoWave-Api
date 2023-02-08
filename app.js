c17f26e8c293af327babd0cbdf7cd566f6dd6d44
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2023-02-08 15:32:23 +0000

    [UPDATE] Function for better understanding

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
+const Octo = require('octonode');
+const client = Octo.client(process.env.GITHUB_TOKEN);
+
+const repo = client.repo('RetrowPoLo/CryptoWave-Api');
 
 // Create a new Express app
 const app = express();
@@ -7,9 +12,37 @@ const app = express();
 // Initialize an array to store the names of missing cryptos
 let missingCryptos = [];
 
+// If the file exists, read the contents and split it into an array of strings
 if (fs.existsSync('missing-cryptos.txt')) {
-	// If the file exists, read the contents and split it into an array of strings
 	missingCryptos = fs.readFileSync('missing-cryptos.txt', 'utf8').split('\n');
+}
+
+// Function to create an issue in the GitHub repository
+function createIssue(icon) {
+	repo.issue(
+		{
+			title: 'Missing crypto icon',
+			body: `[Automatic] Missing crypto icons has been requested : ${icon}`,
+			labels: ['🤖automatic', '❓missing crypto icon'],
+		},
+		(err) => {
+			if (err) {
+				console.log(err);
+			}
+		}
+	);
+}
+
+// Function to write the missingCryptos array to file
+function writeMissingCryptosToFile() {
+	fs.writeFileSync('missing-cryptos.txt', missingCryptos.join('\n'));
+}
+
+// Function to add a missing crypto symbol to the missingCryptos array and write the array to file
+function addMissingCrypto(symbol) {
+	missingCryptos.push(symbol);
+	writeMissingCryptosToFile();
+	createIssue(symbol);
 }
 
 // GET route for the root path
@@ -35,8 +68,7 @@ app.get('/icon/:symbol', async (req, res) => {
 			// If the file does not exist, check if the symbol is already in the missingCryptos array
 			if (!missingCryptos.includes(symbol)) {
 				// If the symbol is not in the array, add it to the array and write the array to the 'missing-cryptos.txt' file
-				missingCryptos.push(symbol);
-				fs.writeFileSync('missing-cryptos.txt', missingCryptos.join('\n'));
+				addMissingCrypto(symbol);
 			}
 			// Send the 'generic.png' icon as a response with cache-control headers set to cache the file for one day
 			res.setHeader('Cache-Control', 'public, max-age=86400');
